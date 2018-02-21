05684d46aba876c41b3b23b62c1d6b79915571f3
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-22 00:25:38 +0530

    Ticker: Fetching Live Data and displaying it in the table

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PriceRow from '../PriceRow';
 import Graph from '../Graph';
 import Ticker from '../Ticker';
 import './index.css';
 
 
-const Body = () => (
-  <div className="Body">
-    <PriceRow />
-    <div className="Body-graph-and-ticker-container">
-      <Graph />
-      <Ticker />
-    </div>
-  </div>
-);
+class Body extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedCoin: 'BTC',
+    };
+  }
+
+  setSelectedCoin(sym) {
+    this.setState({
+      selectedCoin: sym,
+    });
+  }
+
+  render() {
+    return (
+      <div className="Body">
+        <PriceRow />
+        <div className="Body-graph-and-ticker-container">
+          <Graph coin={this.state.selectedCoin} />
+          <Ticker onClick={sym => this.setSelectedCoin(sym)} />
+        </div>
+      </div>
+    );
+  }
+}
 
 
 export default Body;
--- src/components/Ticker/index.jsx
+++ src/components/Ticker/index.jsx
@@ -1,230 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './index.css';
 
 
-const Ticker = () => (
-  <div className="Ticker">
-    <table className="Ticker-table" cellSpacing="0" cellPadding="0">
-      <thead>
-        <th className="Ticker-table-header-th">Symbol</th>
-        <th className="Ticker-table-header-th">Coin Name</th>
-        <th className="Ticker-table-header-th">Price</th>
-        <th className="Ticker-table-header-th">Volume</th>
-        <th className="Ticker-table-header-th">Change</th>
-      </thead>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-      <tr className="Ticker-table-row">
-        <td className="Ticker-table-row-td">BTC</td>
-        <td className="Ticker-table-row-td">Bitcoin</td>
-        <td className="Ticker-table-row-td">7854.86</td>
-        <td className="Ticker-table-row-td">1234599</td>
-        <td className="Ticker-table-row-td">0.0398</td>
-      </tr>
-    </table>
-  </div>
-);
-
+class Ticker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      prices: [],
+    };
+  }
+  componentDidMount() {
+    axios.get('/prices').then((priceData) => {
+      this.setState({
+        prices: priceData.data,
+      });
+    });
+  }
+  render() {
+    return (
+      <div className="Ticker">
+        <table className="Ticker-table" cellSpacing="0" cellPadding="0">
+          <thead>
+            <th className="Ticker-table-header-th">Symbol</th>
+            <th className="Ticker-table-header-th">Coin Name</th>
+            <th className="Ticker-table-header-th">Price</th>
+            <th className="Ticker-table-header-th">Volume</th>
+            <th className="Ticker-table-header-th">Change</th>
+          </thead>
+          {
+            this.state.prices.map(price => (
+              <tr className="Ticker-table-row">
+                <td className="Ticker-table-row-td">{price.Symbol}</td>
+                <td className="Ticker-table-row-td">Bitcoin</td>
+                <td className="Ticker-table-row-td">{price.Price}</td>
+                <td className="Ticker-table-row-td">{price.Volume.toFixed(3)}</td>
+                <td className={price.Change > 0 ? 'Ticker-table-row-td success' : 'Ticker-table-row-td danger'}>{price.Change.toFixed(3)}</td>
+              </tr>
+              ))
+          }
+        </table>
+      </div>
+    );
+  }
+}
 
 export default Ticker;
