a7cdbc5ae4700ec61017cb97314514c02b67fb0e
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-21 23:01:40 +0530

    Graph: Added the target for the Highcharts

--- src/components/Graph/index.jsx
+++ src/components/Graph/index.jsx
@@ -3,7 +3,15 @@ import './index.css';
 
 
 const Graph = () => (
-  <div className="Graph" />
+  <div className="Graph">
+    <div className="Graph-header">
+      <h3>graph header</h3>
+
+    </div>
+    <div id="Graph-Live-Graph" className="Graph-liveGraph">
+        Live graph from highcharts
+    </div>
+  </div>
 );
 
 
