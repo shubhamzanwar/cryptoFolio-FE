95112686853ee610ebaf9d140676e5fc206fae1a
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-21 19:14:44 +0530

    Adding the basic components

---
+++ src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+
+const App = () => (
+  <div className="App">
+    hello
+  </div>
+);
+
+
+export default App;
--- src/render.jsx
+++ src/render.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import App from './components/App';
 
 ReactDOM.render(
-  (
-    <Provider store={store}>
-      <div>
-        <p> Hello </p>
-      </div>
-    </Provider>),
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
