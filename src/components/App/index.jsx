1339970bf0b62da092c02a498187ef84f73b50ae
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-21 21:57:14 +0530

    Adding the body to the App

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header';
+import Body from '../Body';
 import './index.css';
 
 
 const App = () => (
   <div className="App">
     <Header />
+    <Body />
   </div>
 );
 
---
+++ src/components/Body/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PriceRow from '../PriceRow';
+import './index.css';
+
+
+const Body = () => (
+  <div className="Body">
+    <PriceRow />
+  </div>
+);
+
+
+export default Body;
