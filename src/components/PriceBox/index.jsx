af32f233d835ab4e1f8466d9a696e5f2bf700d77
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-21 21:57:25 +0530

    Adding the priceRow to the body

---
+++ src/components/PriceBox/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+
+const PriceBox = () => (
+  <div className="PriceBox" />
+);
+
+
+export default PriceBox;
---
+++ src/components/PriceRow/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PriceBox from '../PriceBox';
+import './index.css';
+
+
+const PriceRow = () => (
+  <div className="PriceRow">
+    <PriceBox />
+  </div>
+);
+
+
+export default PriceRow;
