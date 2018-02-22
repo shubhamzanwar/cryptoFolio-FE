b0e571af3aa793aeec1009d505992fafdef84d60
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-22 19:11:18 +0530

    PriceRow: Adding dummy PriceRow at the top of the homePage. Will be filled dynamically later

--- src/components/PriceRow/index.jsx
+++ src/components/PriceRow/index.jsx
@@ -1,20 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import PriceBox from '../PriceBox';
 import './index.css';
 
 
-const PriceRow = () => (
+const PriceRow = props => (
   <div className="PriceRow">
-    <PriceBox />
-    <PriceBox />
-    <PriceBox />
-    <PriceBox />
-    <PriceBox />
-    <PriceBox />
-    <PriceBox />
-    <PriceBox />
+    {
+      props.boxContent.map(box => <PriceBox box={box} />)
+    }
   </div>
 );
 
+PriceRow.propTypes = {
+  boxContent: PropTypes.arrayOf(),
+};
+
+PriceRow.defaultProps = {
+  boxContent: [
+    {
+      String: 'BTC / EUR',
+      price: 111986,
+      change: 14.90,
+    },
+    {
+      String: 'BTC / USD',
+      price: 119893,
+      change: 3.90,
+    },
+    {
+      String: 'ETH / EUR',
+      price: 4622,
+      change: 1.23,
+    },
+    {
+      String: 'ETH / USD',
+      price: 4525,
+      change: -2.73,
+    },
+    {
+      String: 'ETH / BTC',
+      price: 0.0410417,
+      change: 14.90,
+    },
+    {
+      String: 'ETH / EUR',
+      price: 4622,
+      change: 1.23,
+    },
+    {
+      String: 'ETH / USD',
+      price: 4525,
+      change: -2.73,
+    },
+    {
+      String: 'ETH / BTC',
+      price: 0.0410417,
+      change: 14.90,
+    },
+  ],
+};
 
 export default PriceRow;
