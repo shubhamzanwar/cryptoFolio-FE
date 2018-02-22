b9dbb9edddb8deea307f32737268df5872345f92
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-22 08:56:41 +0530

    TickerRow: Including the Coin name in the display

--- src/components/TickerRow/index.jsx
+++ src/components/TickerRow/index.jsx
@@ -5,7 +5,7 @@ import './index.css';
 const TickerRow = props => (
   <tr className="Ticker-table-row" onClick={() => props.select(props.price.Symbol)}>
     <td className="Ticker-table-row-td">{props.price.Symbol}</td>
-    <td className="Ticker-table-row-td">Bitcoin</td>
+    <td className="Ticker-table-row-td">{props.price.Name}</td>
     <td className="Ticker-table-row-td">{props.price.Price}</td>
     <td className="Ticker-table-row-td">{props.price.Volume.toFixed(3)}</td>
     <td className={props.price.Change > 0 ? 'Ticker-table-row-td success' : 'Ticker-table-row-td danger'}>{props.price.Change.toFixed(3)} <i className={props.price.Change > 0 ? 'fa fa-long-arrow-up' : 'fa fa-long-arrow-down'} /></td>
@@ -15,6 +15,7 @@ const TickerRow = props => (
 TickerRow.propTypes = {
   price: PropTypes.shape({
     Symbol: PropTypes.string.isRequired,
+    Name: PropTypes.string.isRequired,
     Price: PropTypes.number.isRequired,
     Volume: PropTypes.number.isRequired,
     Change: PropTypes.number.isRequired,
