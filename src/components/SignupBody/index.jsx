e6d28b31d6242ae71d067102d63d2082d8743262
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-23 13:34:08 +0530

    SignupBody: Listing the features

--- src/components/SignupBody/index.jsx
+++ src/components/SignupBody/index.jsx
@@ -40,42 +40,20 @@ class SignupBody extends Component {
             Why Should I Register ?
             </div>
             <div className="signup-bullets">
-            <ul>
-            <li>
-              <span className="signup-list">Live ticker and graph:</span>
-              Stay updated with the near-real-time values movement<br />
-              of cryptocoins and get the visualization of data for 3 periods.
-            </li>
-            <li>
-              <span className="signup-list">Portfolio:</span>
-              Create your personalised portfolio of cryptocoins and<br />
-              see its value as per current market prices. 
-            </li>
-            <li>
-              <span className="signup-list">Twitter Feed:</span>
-              Get what eveyone is talking about the cryptocoins<br />
-              on twitter in live twitter feed column.
-            </li>
-            <li>
-              <span className="signup-list">Mark your favroite coins:</span>
-              Find your favroite coins always on the top of coins list<br />
-              so that you can save your scroll time.
-            </li> 
-          </ul>
-             // <ul>
-             //   <li>
-             // Get real time updates about your favortie coins<br />
-             // with our live ticker and graph feature.
-             //   </li>
-             //   <li>
-             // See What people are talking about<br />
-             // Twitter feed from the official handles for the coins of your choice.
-             //  </li>
-              //  <li>
-              //Manage your investments<br />
-              //Create a free portfolio to keep track of the coins you own.
-                //</li>
-              //</ul>
+              <ul>
+                <li>
+              Get real time updates about your favortie coins<br />
+              with our live ticker and graph feature.
+                </li>
+                <li>
+              See What people are talking about<br />
+              Twitter feed from the official handles for the coins of your choice.
+               </li>
+                <li>
+              Manage your investments<br />
+              Create a free portfolio to keep track of the coins you own.
+                </li>
+              </ul>
             </div>
           </div>
           <div className="signup-form-div">
