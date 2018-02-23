dcb7eb06eea0c919664bbe327e12cbf4718a77b3
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-23 13:51:28 +0530

    App: Changing the navigation state

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -9,26 +9,20 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      homepage: 1,
-      activeButton: 0,
+      homepage: true,
     };
   }
-  changeActiveButton = (value) => {
-    this.setState({
-      activeButton: value,
-    });
-  }
-  navigatePage = (page) => {
+  navigatePage(page) {
     this.setState({
       homepage: page,
-    })
+    });
   }
   render() {
     return (
       <div className="App">
-        <Header active={this.state.activeButton} navigatePage={(page)=> this.navigatePage(page)}/>
-        { 
-          this.state.homepage ? <Body /> : <SignupBody button={(value)=> this.changeActiveButton(value)}/>
+        <Header active={this.state.homepage} navigatePage={page => this.navigatePage(page)} />
+        {
+          this.state.homepage ? <Body /> : <SignupBody />
         }
       </div>
     );
