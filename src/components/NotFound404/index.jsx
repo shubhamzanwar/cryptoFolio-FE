8c3c77932f83528d3cf1bc8b323517c7f995e00f
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-24 16:29:24 +0530

    NotFound: adding a button to return to the homepage

--- src/components/NotFound404/index.jsx
+++ src/components/NotFound404/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 const NotFound = () => (
   <div className="NotFound">
     <h2 className="NotFound-error-code">Error 404</h2>
     <h3 className="NotFound-error-message">The Page you were looking for was not Found</h3>
+    <Link className="NotFound-go-back-home-button" to="/">
+      Go back home?
+    </Link>
   </div>
 );
 
