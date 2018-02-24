6dc63c4d7881785689c0c6f2757b82a5c4f23d5d
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-24 15:52:48 +0530

    NotFound: Creating the not found page to display when the user navigates to an incorrect link

---
+++ src/components/NotFound404/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+const NotFound = () => (
+  <div className="NotFound">
+    <h2 className="NotFound-error-code">Error 404</h2>
+    <h3 className="NotFound-error-message">The Page you were looking for was not Found</h3>
+  </div>
+);
+
+export default NotFound;
