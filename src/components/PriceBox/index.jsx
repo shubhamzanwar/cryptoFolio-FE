479bf4332d48f882843b46eba87ac939bf875680
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-02-24 18:25:14 +0530

    PriceBox: including units in the prices

--- src/components/PriceBox/index.jsx
+++ src/components/PriceBox/index.jsx
@@ -6,7 +6,7 @@ import './index.css';
 const PriceBox = props => (
   <div className="PriceBox">
     <h3>{props.box.Symbol} / USD <i className={props.box.Change > 0 ? 'fa fa-long-arrow-up PriceBox-change-success' : 'fa fa-long-arrow-down PriceBox-change-danger'} /></h3>
-    <p className="PriceBox-price">{props.box.Price} <span className={props.box.Change > 0 ? 'PriceBox-change PriceBox-change-success' : 'PriceBox-change PriceBox-change-danger'} >{props.box.Change.toFixed(3)}%</span></p>
+    <p className="PriceBox-price">{props.box.Price} USD<span className={props.box.Change > 0 ? 'PriceBox-change PriceBox-change-success' : 'PriceBox-change PriceBox-change-danger'} >{props.box.Change.toFixed(3)}%</span></p>
   </div>
 );
 
