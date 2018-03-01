c1a8ad100cb94f2b01135780efc2432f31c5c66c
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-01 18:54:07 +0530

    Initial Portfolio Page Layout

---
+++ src/components/Investment/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Investment extends Component {
+  render() {
+    return (
+      <div>
+        <p>Investment</p>
+      </div>
+    );
+  }
+}
+
+
+export default Investment;
---
+++ src/components/PortfolioDistribution/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class PortfolioDistribution extends Component {
+  render() {
+    return (
+      <div>
+        <p>Portfolio Distribution</p>
+      </div>
+    );
+  }
+}
+
+
+export default PortfolioDistribution;
