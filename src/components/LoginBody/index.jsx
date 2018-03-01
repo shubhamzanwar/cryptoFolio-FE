d0769921e3bb2cb5b798d6ce938980ca6220bb17
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-03-01 19:13:29 +0530

    LoginForm: Creating the loginform view

--- src/components/LoginBody/index.jsx
+++ src/components/LoginBody/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import LoginForm from '../LoginForm';
 import './index.css';
 
 
 const LoginBody = () => (
   <div className="LoginBody">
-    Login Body
+    <LoginForm />
   </div>
 );
 
