377d4763f87c042242def155f9bc2fea44370bfd
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-03-01 20:40:06 +0530

    LoginForm: Storing the email and password in state and making the login request

--- src/components/LoginBody/index.jsx
+++ src/components/LoginBody/index.jsx
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import LoginForm from '../LoginForm';
 import './index.css';
 
 
-const LoginBody = () => (
-  <div className="LoginBody">
-    <LoginForm />
-  </div>
-);
+class LoginBody extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: '', // will be added to the store
+      name: '', // will be added to the store
+      message: '',
+    };
+  }
+  login(email, password) {
+    const options = {
+      url: '/login',
+      method: 'POST',
+      data: {
+        email,
+        password,
+      },
+    };
+    axios(options)
+      .then((response) => {
+        if (response.data.code === 200) {
+          this.setState({
+            token: response.data.token,
+            name: response.data.fullName,
+            message: 'success',
+          });
+        } else {
+          this.setState({
+            message: response.data.message,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  render() {
+    return (
+      <div className="LoginBody">
+        <div className="LoginBody-tint">
+          <LoginForm
+            message={this.state.message}
+            login={(email, password) => this.login(email, password)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 
 export default LoginBody;
