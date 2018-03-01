43910ad6be3465bf9225d6fd015102f0ab9103d9
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-01 21:05:45 +0530

    Add Investment

--- src/components/Investment/index.jsx
+++ src/components/Investment/index.jsx
@@ -4,8 +4,21 @@ import './index.css';
 class Investment extends Component {
   render() {
     return (
-      <div>
-        <p>Investment</p>
+      <div className="Investment">
+        <div className="Investment-Header">
+          <p className="Investment-Header-Title">Ethereum</p>
+        </div>
+        <div>
+          <div className="Investment-Values">
+            <p className="Investment-Invested">Invested<br /><span className="Investment-Invested-value">$ 27,000</span></p>
+            <p className="Investment-Current">Invested<br /><span className="Investment-Current-value">$ 27,000</span></p>
+          </div>
+          <div className="Investment-Graph1">
+            <div className="Investment-bar">
+              <div className="Investment-progress" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
