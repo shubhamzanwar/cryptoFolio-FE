59ea402b8f29707427d64dc524432ca6c3490c36
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-01 22:56:51 +0530

    Add Portfolio Distribution

--- src/components/PortfolioDistribution/index.jsx
+++ src/components/PortfolioDistribution/index.jsx
@@ -4,8 +4,20 @@ import './index.css';
 class PortfolioDistribution extends Component {
   render() {
     return (
-      <div>
-        <p>Portfolio Distribution</p>
+      <div className="Distribution">
+        <div className="Distribution-Header">
+          <p className="Distribution-Header-Title">My Coins</p>
+        </div>
+        <div className="Distribution-GraphContainer">
+          <div className="Graph" />
+          <div className="GraphLabel">
+            <div className="GraphLabelItem">
+              <span>42%<br />BCH</span>
+              <span>42%<br />BCH</span><br />
+              <span>42%<br />BCH</span><br />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
