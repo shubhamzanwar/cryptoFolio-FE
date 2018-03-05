4066fefd78e81baaa989386fa65f2c9fa972e6db
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-05 18:49:22 +0530

    Add blank Add Coin Modal

---
+++ src/components/AddCoinModal/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+const AddCoinModel = () => (
+  <div className="AddCoinModal">
+    <span className="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+);
+
+AddCoinModel.propTypes = {
+};
+
+AddCoinModel.defaultProps = {
+};
+
+export default AddCoinModel;
