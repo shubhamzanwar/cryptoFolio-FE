7af071a6bf7cc371156b674f3417716fd16bd83b
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-03-07 17:38:34 +0530

    Signup: error message div layout add as component

--- src/components/SignupBody/index.jsx
+++ src/components/SignupBody/index.jsx
@@ -1,36 +1,48 @@
 import React, { Component } from 'react';
 import './index.css';
+import ErrorMessage from '../ErrorMessage';
 
-const registerUser = (e) => {
-  e.preventDefault();
-  const data = new FormData(e.target);
-  const fullName = data.get('fullname');
-  const email = data.get('email');
-  const password = data.get('password');
-  const confirmPassword = data.get('confirmpassword');
-  const mobileNumbe = data.get('contact');
-  if (password === confirmPassword) {
-    const payload = {
-      fullName,
-      email,
-      password,
-      confirmPassword,
-      mobileNumbe,
-    };
-    fetch('/signup', {
-      method: 'POST',
-      body: JSON.stringify(payload),
-    })
-      .then((response) => {
-        if (response.status === 201) {
-          return 0;
-        }
-        return null;
-      });
-  }
-};
 
 class SignupBody extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+    };
+  }
+  registerUser = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const fullName = data.get('fullname');
+    const email = data.get('email');
+    const password = data.get('password');
+    const confirmPassword = data.get('confirmpassword');
+    const mobileNumbe = data.get('contact');
+    if (password !== confirmPassword) {
+      this.setState({
+        error: 'Ops! Password Mismatch',
+      })
+    } else {
+      const payload = {
+        fullName,
+        email,
+        password,
+        confirmPassword,
+        mobileNumbe,
+      };
+      fetch('/signup', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+      })
+        .then((response) => {
+          if (response.status === 201) {
+            return 0;
+          }
+          return null;
+        });
+    }
+  };
+
   render() {
     return (
       <div className="signup-body-div">
@@ -57,9 +69,13 @@ class SignupBody extends Component {
             </div>
           </div>
           <div className="signup-form-div">
-            <form className="signup-form" onSubmit={registerUser}>
+            <form className="signup-form" onSubmit={this.registerUser}>
               <div className="signup-form-padding">
-                <div className="signup-register">Register</div><br />
+              <div className="signup-register">Register</div><br />
+              {this.state.error ? 
+                <ErrorMessage message={this.state.error} />
+                : ''                
+              }
                 <input
                   type="text"
                   name="fullname"
