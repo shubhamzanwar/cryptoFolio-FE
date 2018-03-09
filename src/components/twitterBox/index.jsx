6478590793d739badba9a5e71690018c1ae58f31
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-03-09 15:21:01 +0530

    TwitterBox: Using the coin prop to fetch tweets relevant to the coin

--- src/components/twitterBox/index.jsx
+++ src/components/twitterBox/index.jsx
@@ -1,44 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 import TwitterPost from './TwitterPost';
 import './index.css';
 
-const tweets = [{
-  content: 'And then they have the audacity to say that Bitcoin Cash is trying to "steal the Bitcoin brand."\nWhy does Bitcoin… \nhttps://t.co/CNfuVMWBhu',
-  screenName: 'Bitcoin',
-  fullName: 'Bitcoin',
-  profPic: 'http://pbs.twimg.com/profile_images/421692600446619648/dWAbC2wg_normal.jpeg',
-}, {
-  content: 'And then they have the audacity to say that Bitcoin Cash is trying to "steal the Bitcoin brand."\nWhy does Bitcoin… \nhttps://t.co/CNfuVMWBhu',
-  screenName: 'Bitcoin',
-  fullName: 'Bitcoin',
-  profPic: 'http://pbs.twimg.com/profile_images/421692600446619648/dWAbC2wg_normal.jpeg',
-}, {
-  content: 'And then they have the audacity to say that Bitcoin Cash is trying to "steal the Bitcoin brand."\nWhy does Bitcoin… \nhttps://t.co/CNfuVMWBhu',
-  screenName: 'Bitcoin',
-  fullName: 'Bitcoin',
-  profPic: 'http://pbs.twimg.com/profile_images/421692600446619648/dWAbC2wg_normal.jpeg',
-}, {
-  content: 'And then they have the audacity to say that Bitcoin Cash is trying to "steal the Bitcoin brand."\nWhy does Bitcoin… \nhttps://t.co/CNfuVMWBhu',
-  screenName: 'Bitcoin',
-  fullName: 'Bitcoin',
-  profPic: 'http://pbs.twimg.com/profile_images/421692600446619648/dWAbC2wg_normal.jpeg',
-}];
-
-
-const TwitterBox = () => (
-  <div className="TwitterBox">
-    <h3>Twitter <i className="fab fa-twitter TwitterBox-icon" /></h3>
-    <div className="TwitterBox-posts">
-      {
-          tweets.map(tweet => (
+class TwitterBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tweets: [],
+    };
+    this.getTweets(this.props.coin);
+  }
+  componentWillUpdate(nextProps) {
+    if (nextProps.coin !== this.props.coin) {
+      this.getTweets(nextProps.coin);
+    }
+  }
+  getTweets(coin) {
+    axios.get(`/twitter/${coin}`).then((tweets) => {
+      this.setState({
+        tweets: tweets.data,
+      });
+    });
+  }
+  render() {
+    return (
+      <div className="TwitterBox">
+        <h3>Twitter <i className="fab fa-twitter TwitterBox-icon" /></h3>
+        <div className="TwitterBox-posts">
+          {
+          (this.state.tweets).map(tweet => (
             <div>
               <TwitterPost tweet={tweet} />
               <hr className="Twitter-divider" />
             </div>))
       }
-    </div>
-  </div>
-);
+        </div>
+      </div>
+    );
+  }
+}
+
+TwitterBox.propTypes = {
+  coin: PropTypes.string,
+};
+
+TwitterBox.defaultProps = {
+  coin: 'BTC',
+};
 
 export default TwitterBox;
 
