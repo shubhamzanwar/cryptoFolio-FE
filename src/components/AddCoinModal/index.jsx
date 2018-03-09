04faf63f924a680cd594ae3c6ad8c26e1fd8642f
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-09 16:32:09 +0530

    Add AddCoin Form and Style

--- src/components/AddCoinModal/index.jsx
+++ src/components/AddCoinModal/index.jsx
@@ -3,24 +3,24 @@ import Modal from 'react-responsive-modal';
 import PropTypes from 'prop-types';
 import './index.css';
 
-const AddCoinModel = props => (
-  <Modal open={props.state} onClose={props.onCloseModal} little>
+const AddCoinModal = props => (
+  <Modal open={props.state} onClose={props.onCloseModal} little styles={{ modal: { backgroundColor: 'rgb(0, 164, 236)' } }}>
     <h2>Add coin</h2>
-    <form>
-      <input type="text" placeholder="Coin Name" /><br />
-      <input type="text" placeholder="Quantity" /><br />
-      <input type="text" placeholder="Purchased Price" /><br />
-      <button type="submit">Add Coin</button>
+    <form className="addCoinModal_addCoinForm">
+      <input className="addCoinModal_addCoinForm_input" type="text" placeholder="Coin Name" /><br />
+      <input className="addCoinModal_addCoinForm_input" type="text" placeholder="Quantity" /><br />
+      <input className="addCoinModal_addCoinForm_input" type="text" placeholder="Purchased Price" /><br />
+      <button className="addCoinModal_addCoinForm_submit" type="submit">Add Coin</button>
     </form>
   </Modal>
 );
 
-AddCoinModel.propTypes = {
+AddCoinModal.propTypes = {
   onCloseModal: PropTypes.func.isRequired,
   state: PropTypes.string.isRequired,
 };
 
-AddCoinModel.defaultProps = {
+AddCoinModal.defaultProps = {
 };
 
-export default AddCoinModel;
+export default AddCoinModal;
