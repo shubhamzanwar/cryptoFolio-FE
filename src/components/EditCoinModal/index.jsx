d1d2e6685eae0e89346c59e6f9fdb35381d8602b
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-12 03:50:25 +0530

    Add Props in EditCoin

--- src/components/EditCoinModal/index.jsx
+++ src/components/EditCoinModal/index.jsx
@@ -7,9 +7,9 @@ const EditCoinModal = props => (
   <Modal open={props.state} onClose={props.onCloseModal} little styles={{ modal: { backgroundColor: 'rgb(0, 164, 236)' } }}>
     <h2>Edit coin</h2>
     <form className="editCoinModal_editCoinForm">
-      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coindName">Coin Name<input className="editCoinModal_editCoinForm_input" id="coindName" type="text" placeholder="Coin Name" /></label><br />
-      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coinPurchasedPrice">Quantity<input className="editCoinModal_editCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Quantity" /></label><br />
-      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coinPurchasedPrice">Purchased Price<input className="editCoinModal_editCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Purchased Price" /></label><br />
+      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coindName">Coin Name<input className="editCoinModal_editCoinForm_input" id="coindName" type="text" placeholder="Coin Name" value={props.data.name} /></label><br />
+      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coinPurchasedPrice">Quantity<input className="editCoinModal_editCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Quantity" value={props.data.quantity} /></label><br />
+      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coinPurchasedPrice">Purchased Price<input className="editCoinModal_editCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Purchased Price" value={props.data.price} /></label><br />
       <button className="addCoinModal_addCoinForm_submit" type="submit">Update</button>
     </form>
   </Modal>
@@ -18,9 +18,11 @@ const EditCoinModal = props => (
 EditCoinModal.propTypes = {
   onCloseModal: PropTypes.func.isRequired,
   state: PropTypes.string.isRequired,
-};
-
-EditCoinModal.defaultProps = {
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    quantity: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default EditCoinModal;
