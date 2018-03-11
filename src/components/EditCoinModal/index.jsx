203016bb49645a6ecfe82ce0df7f767973f54046
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-12 03:59:35 +0530

    Add delete coin button

--- src/components/EditCoinModal/index.jsx
+++ src/components/EditCoinModal/index.jsx
@@ -5,12 +5,12 @@ import './index.css';
 
 const EditCoinModal = props => (
   <Modal open={props.state} onClose={props.onCloseModal} little styles={{ modal: { backgroundColor: 'rgb(0, 164, 236)' } }}>
-    <h2>Edit coin</h2>
+    <h2>Edit coin</h2><button className="editCoinModal_editCoinForm_delete" type="submit">Delete Coin</button>
     <form className="editCoinModal_editCoinForm">
       <label className="editCoinModal_editCoinForm_input_label" htmlFor="coindName">Coin Name<input className="editCoinModal_editCoinForm_input" id="coindName" type="text" placeholder="Coin Name" value={props.data.name} /></label><br />
       <label className="editCoinModal_editCoinForm_input_label" htmlFor="coinPurchasedPrice">Quantity<input className="editCoinModal_editCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Quantity" value={props.data.quantity} /></label><br />
       <label className="editCoinModal_editCoinForm_input_label" htmlFor="coinPurchasedPrice">Purchased Price<input className="editCoinModal_editCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Purchased Price" value={props.data.price} /></label><br />
-      <button className="addCoinModal_addCoinForm_submit" type="submit">Update</button>
+      <button className="editCoinModal_editCoinForm_submit" type="submit">Update</button>
     </form>
   </Modal>
 );
