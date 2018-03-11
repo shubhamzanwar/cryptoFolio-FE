bf03f9fc45ba2870c9c8dd96017ead8f42b0bc3f
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-12 03:35:07 +0530

    Add labels to Add Coin Model

--- src/components/AddCoinModal/index.jsx
+++ src/components/AddCoinModal/index.jsx
@@ -7,11 +7,11 @@ const AddCoinModal = props => (
   <Modal open={props.state} onClose={props.onCloseModal} little styles={{ modal: { backgroundColor: 'rgb(0, 164, 236)' } }}>
     <h2>Add coin</h2>
     <form className="addCoinModal_addCoinForm">
-      <input className="addCoinModal_addCoinForm_input" type="text" placeholder="Coin Name" /><br />
-      <input className="addCoinModal_addCoinForm_input" type="text" placeholder="Quantity" /><br />
-      <input className="addCoinModal_addCoinForm_input" type="text" placeholder="Purchased Price" /><br />
+      <label className="addCoinModal_addCoinForm_input_label" htmlFor="coindName">Coin Name<input className="addCoinModal_addCoinForm_input" id="coindName" type="text" placeholder="Coin Name" /></label><br />
+      <label className="addCoinModal_addCoinForm_input_label" htmlFor="coinPurchasedPrice">Quantity<input className="addCoinModal_addCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Quantity" /></label><br />
+      <label className="addCoinModal_addCoinForm_input_label" htmlFor="coinPurchasedPrice">Purchased Price<input className="addCoinModal_addCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Purchased Price" /></label><br />
       <button className="addCoinModal_addCoinForm_submit" type="submit">Add Coin</button>
-    </form>
+    </form>g
   </Modal>
 );
 
