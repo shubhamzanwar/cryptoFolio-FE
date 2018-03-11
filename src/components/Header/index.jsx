a4fe6a209555eae60111229e2ae1b3a28d363ab3
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-03-12 00:11:38 +0530

    Logout: Adding the logout functionality to the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 import './index.css';
 
-const Header = () => (
-  <header className="Header">
-    <div className="Header-container">
-      <Link className="Header-home-link" to="/">
-        <h3
-          className="Header-heading"
-        >
-          <i className="fa fa-briefcase" aria-hidden="true" /> CryptoFolio
-        </h3>
-      </Link>
-      {
-          window.localStorage.getItem('cryptologgedin') ?
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedin: window.localStorage.getItem('cryptologgedin') === 'true',
+    };
+  }
+  logout() {
+    window.localStorage.setItem('cryptologgedin', false);
+    window.localStorage.setItem('cryptotoken', null);
+    window.localStorage.setItem('cryptousername', null);
+    this.setState({
+      loggedin: false,
+    });
+  }
+  render() {
+    return (
+      <header className="Header">
+        <div className="Header-container">
+          <Link className="Header-home-link" to="/">
+            <h3
+              className="Header-heading"
+            >
+              <i className="fa fa-briefcase" aria-hidden="true" /> CryptoFolio
+            </h3>
+          </Link>
+          {
+          this.state.loggedin ?
             <div className="Header-user-bar">
               <div className="Header-button-container">
                 <NavLink
@@ -33,7 +50,17 @@ const Header = () => (
                   <p>Portfolio</p>
                 </NavLink>
               </div>
-              <p>{window.localStorage.getItem('cryptousername')}</p>
+              <div className="Header-user">
+                <p>{window.localStorage.getItem('cryptousername')} <i className="fas fa-caret-down" /></p>
+                <div className="Header-logout-link">
+                  <button
+                    className="Header-logout-button"
+                    onClick={() => this.logout()}
+                  ><p>Logout </p><i className="fas fa-sign-out-alt" />
+                  </button>
+                </div>
+              </div>
+
             </div> :
             <div className="Header-button-container">
               <NavLink
@@ -53,8 +80,10 @@ const Header = () => (
             </div>
         }
 
-    </div>
-  </header>
-);
+        </div>
+      </header>
+    );
+  }
+}
 
 export default Header;
