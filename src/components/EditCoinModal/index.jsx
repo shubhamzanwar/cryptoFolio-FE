6f558c5bb74d9b7cc0148c35b4efa1b3f85e04f2
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-12 11:26:37 +0530

    FIX: Add/Modify Model

--- src/components/EditCoinModal/index.jsx
+++ src/components/EditCoinModal/index.jsx
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 import './index.css';
 
 const EditCoinModal = props => (
-  <Modal open={props.state} onClose={props.onCloseModal} little styles={{ modal: { backgroundColor: 'rgb(0, 164, 236)' } }}>
-    <h2>Edit coin</h2><button className="editCoinModal_editCoinForm_delete" type="submit">Delete Coin</button>
+  <Modal open={props.state} onClose={props.onCloseModal} little styles={{ modal: { backgroundColor: 'rgb(255, 255, 255)', borderRadius: '10px' } }}>
+    <h2 className="editCoinModal_header">Edit coin</h2><button className="editCoinModal_editCoinForm_delete" type="submit">Delete Coin</button>
     <form className="editCoinModal_editCoinForm">
-      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coindName">Coin Name<input className="editCoinModal_editCoinForm_input" id="coindName" type="text" placeholder="Coin Name" value={props.data.name} /></label><br />
-      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coinPurchasedPrice">Quantity<input className="editCoinModal_editCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Quantity" value={props.data.quantity} /></label><br />
-      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coinPurchasedPrice">Purchased Price<input className="editCoinModal_editCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Purchased Price" value={props.data.price} /></label><br />
+      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coindName">Coin Name<input className="editCoinModal_editCoinForm_input" id="coindName" type="text" placeholder="Coin Name" value={props.data.name} /></label>
+      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coinPurchasedPrice">Quantity<input className="editCoinModal_editCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Quantity" value={props.data.quantity} /></label>
+      <label className="editCoinModal_editCoinForm_input_label" htmlFor="coinPurchasedPrice">Purchased Price<input className="editCoinModal_editCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Purchased Price" value={props.data.price} /></label>
       <button className="editCoinModal_editCoinForm_submit" type="submit">Update</button>
     </form>
   </Modal>
