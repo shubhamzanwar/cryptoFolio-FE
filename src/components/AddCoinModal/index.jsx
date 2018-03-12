1a2b02a78e6d5cbae3c28b39ef53c967e8c455bf
Rahul Kumar <rahulcomp24@gmail.com>
2018-03-12 09:20:41 +0530

    Check for modifyType for header

--- src/components/AddCoinModal/index.jsx
+++ src/components/AddCoinModal/index.jsx
@@ -5,7 +5,7 @@ import './index.css';
 
 const AddCoinModal = props => (
   <Modal open={props.state} onClose={props.onCloseModal} little styles={{ modal: { backgroundColor: 'rgb(0, 164, 236)' } }}>
-    <h2>Add coin</h2>
+    <h2>{props.modifyType === 'addCoin' ? 'Add Coin' : 'Remove Coin'}</h2>
     <form className="addCoinModal_addCoinForm">
       <label className="addCoinModal_addCoinForm_input_label" htmlFor="coindName">Coin Name<input className="addCoinModal_addCoinForm_input" id="coindName" type="text" placeholder="Coin Name" /></label><br />
       <label className="addCoinModal_addCoinForm_input_label" htmlFor="coinPurchasedPrice">Quantity<input className="addCoinModal_addCoinForm_input" id="coinPurchasedPrice" type="number" placeholder="Quantity" /></label><br />
