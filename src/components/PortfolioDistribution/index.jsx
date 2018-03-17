befd4693f5703f2fb7e318a181028f8e770e1402
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-03-17 18:15:55 +0530

    viewPortfolio: cleanup the code

--- src/components/PortfolioDistribution/index.jsx
+++ src/components/PortfolioDistribution/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PortfolioDistributionChart from '../PortifolioDistributionChart';
-import PortfolioDistributionLabel from '../PortofolioDistributionLabel';
 import './index.css';
 
 
