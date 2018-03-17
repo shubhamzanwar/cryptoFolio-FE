e22ba494248ce6024187baf6096f8905c48c5760
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-03-17 15:53:07 +0530

    ViewPortfolio: total sold value add

--- src/utils/helpers/summarizeTransactions.js
+++ src/utils/helpers/summarizeTransactions.js
@@ -1,27 +1,33 @@
 const summarizeTransactions = (transactionsObject) => {
   let totalInvested = 0;
+  let totalSold = 0;
   const transactions = Object.values(transactionsObject).map((coinTransactions) => {
     const { coinName } = coinTransactions[0];
     const { coinSymbol } = coinTransactions[0];
     let quantity = 0;
     let invested = 0;
+    let sold = 0;
 
     coinTransactions.forEach((transaction) => {
       quantity += transaction.quantity;
-      invested += transaction.quantity * transaction.price;
+      if (transaction.quantity > 0) {
+        invested += transaction.quantity * transaction.price;
+      } else sold += transaction.quantity * transaction.price * -1;
     });
     totalInvested += invested;
+    totalSold += sold;
     return {
       coinName,
       coinSymbol,
       quantity,
       invested,
+      sold,
     };
   });
   const filteredTransactions = Object.values(transactions).filter((transaction) => {
     if (transaction.quantity > 0) return true;
     return false;
   });
-  return [filteredTransactions, totalInvested];
+  return [filteredTransactions, totalInvested, totalSold];
 };
 export default summarizeTransactions;
