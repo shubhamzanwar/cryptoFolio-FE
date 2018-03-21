7cce5099589d184f85c8200fd56ae06c16ca235f
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-03-21 16:56:22 +0530

    viewPortfolio: 3 coloums per row in labels

--- src/components/PortfolioDistribution/index.jsx
+++ src/components/PortfolioDistribution/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PortfolioDistributionChart from '../PortifolioDistributionChart';
+import PortfolioDistributionLabel from '../PortofolioDistributionLabel';
+import colors from '../../utils/constants/chartColors'; 
 import './index.css';
 
 
@@ -18,9 +20,10 @@ class PortfolioDistribution extends Component {
   }
 
   conditionInput(trans) {
-    const pieData = trans.map(transaction => ({
+    const pieData = trans.map((transaction, index) => ({
       name: transaction.coinName,
       y: transaction[this.state.displayType],
+      color: colors[index]
     }));
     this.setState({
       pieData,
@@ -47,6 +50,17 @@ class PortfolioDistribution extends Component {
         <div className="Distribution-GraphContainer">
           <div className="Graph">
             <PortfolioDistributionChart portfolioData={this.state.pieData} />
+          </div>
+          <div className="GraphLabel">
+            <div className="GraphLabelItemContainer">
+              { this.state.pieData.map((coin, index) => (
+                <PortfolioDistributionLabel
+                  value={coin.y}
+                  symbol={coin.name}
+                  color={colors[index]}
+                />))
+              }
+            </div>
           </div>
         </div>
       </div>
