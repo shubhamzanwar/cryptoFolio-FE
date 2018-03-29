3d8200308f030f390ed4546eeff778b584bcd9ec
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-03-29 14:54:14 +0530

    App: Adding the Alert Component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
+import Alert from 'react-s-alert';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/jelly.css';
 import Header from '../Header';
 import SignupBody from '../SignupBody';
 import Body from '../Body';
 import LoginBody from '../LoginBody';
 import Portfolio from '../Portfolio';
+import MyCustomContentTemplate from './MyCustomContentTemplate';
 import TransfersBody from '../TransfersBody';
 import NotFound from '../NotFound404';
 import './index.css';
@@ -20,6 +24,7 @@ const App = () => (
       <Route path="/transfers" component={TransfersBody} />
       <Route path="*" exact component={NotFound} />
     </Switch>
+    <Alert stack timeout="none" contentTemplate={MyCustomContentTemplate} />
   </div>
 );
 
