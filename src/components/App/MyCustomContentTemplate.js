93022efa542d0f4dc83404a81eddb38708c6a202
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-03-29 14:52:59 +0530

    Alerts: Custom Template for alerts

---
+++ src/components/App/MyCustomContentTemplate.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Alert from 'react-s-alert';
+
+const MyCustomContentTemplate = ({
+  id,
+  classNames,
+  styles,
+  message,
+  customFields,
+  handleClose,
+}) => {
+  const handleConfirm = () => {
+    customFields.onClick();
+    Alert.close(id);
+  };
+  return (
+    <div className={classNames} id={id} style={styles}>
+      <div className="s-alert-box-inner">
+        {message}
+      </div>
+      <button className="customButton" style={customFields.button ? {} : { display: 'none' }} onClick={handleConfirm.bind(this)}>Login</button>
+      <span className="s-alert-close" onClick={handleClose} />
+    </div>
+  );
+};
+
+export default MyCustomContentTemplate;
